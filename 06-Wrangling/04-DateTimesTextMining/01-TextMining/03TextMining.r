dcae8ed3c638c990adcf243e43bc1ff49c4fdbd4
imoralesgt <ivan.rene.morales@gmail.com>
2023-08-04 23:58:18 +0200

    Finished assessments on sentiment analysis

--- 06-Wrangling/04-DateTimesTextMining/01-TextMining/03TextMining.r
+++ 06-Wrangling/04-DateTimesTextMining/01-TextMining/03TextMining.r
@@ -21,3 +21,32 @@ book %>% unnest_tokens(word, text) %>% select(word) %>%
   filter(!word %in% stop_words$word) %>%
   filter(!str_detect(word, "\\d")) %>%
   count()
+
+
+book %>% unnest_tokens(word, text) %>% select(word) %>%
+  filter(!word %in% stop_words$word) %>%
+  filter(!str_detect(word, "\\d")) %>% count(word) %>%
+  filter(n > 100) %>% count() 
+
+
+
+book %>% unnest_tokens(word, text) %>% select(word) %>%
+  filter(!word %in% stop_words$word) %>%
+  filter(!str_detect(word, "\\d")) %>% count(word) %>%
+  filter(n > 100) %>% arrange(desc(n)) 
+
+# ========================================
+
+words <- book %>% unnest_tokens(word, text) %>% 
+          filter(!word %in% stop_words$word) %>%
+          filter(!str_detect(word, "\\d"))
+
+afinn <- get_sentiments("afinn")
+
+afinn_sentiments <- words %>% inner_join(afinn, by = 'word') %>%
+  select(word, value)
+
+afinn_sentiments_positive <- afinn_sentiments %>% filter(value > 0) %>% count()
+afinn_positive_proportion <- afinn_sentiments_positive / nrow(afinn_sentiments)
+afinn_sentiments_value4 <- afinn_sentiments %>% filter(value == 4) %>% count()
+
